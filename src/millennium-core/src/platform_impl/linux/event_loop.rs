4bd59905af32381cf8552480cdf30f310a20d4a3
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-03-07 12:28:00 -0600

    feat(main): run cleanup code on relaunch
    Upstream from tauri: 9c65abc

--- src/millennium-core/src/platform_impl/linux/event_loop.rs
+++ src/millennium-core/src/platform_impl/linux/event_loop.rs
@@ -14,6 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::new_without_default)]
+
 use std::{
 	cell::RefCell,
 	collections::{HashSet, VecDeque},
