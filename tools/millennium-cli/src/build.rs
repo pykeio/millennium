26b9a4ecbef4f52bc912cbc5fd9b031ad1632c23
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-06-08 12:01:25 -0500

    fix(cli): add missing cfg directive

--- tools/millennium-cli/src/build.rs
+++ tools/millennium-cli/src/build.rs
@@ -275,6 +275,7 @@ pub fn command(options: Options) -> Result<()> {
 			.with_context(|| "failed to build bundler settings")?;
 
 		// set env vars used by the bundler
+		#[cfg(target_os = "linux")]
 		{
 			if matches!(config_.millennium.allowlist.shell.open, ShellAllowlistOpen::Flag(true) | ShellAllowlistOpen::Validate(_)) {
 				std::env::set_var("APPIMAGE_BUNDLE_XDG_OPEN", "1");
