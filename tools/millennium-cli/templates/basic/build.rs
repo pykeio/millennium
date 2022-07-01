d4b345b9183777e7f549ae86712c268bf4138baa
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-06-30 20:46:51 -0500

    refactor(cli): update basic template build.rs

--- tools/millennium-cli/templates/basic/build.rs
+++ tools/millennium-cli/templates/basic/build.rs
@@ -1,7 +1,3 @@
-use millennium_build::{try_build, Attributes, WindowsAttributes};
-
 fn main() {
-	if let Err(error) = try_build(Attributes::new().windows_attributes(WindowsAttributes::new().window_icon_path("../../.icons/icon.ico"))) {
-		panic!("error found during millennium-build: {:#?}", error);
-	}
+	millennium_build::build()
 }
