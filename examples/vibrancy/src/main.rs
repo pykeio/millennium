dfd5d0b2ec1d22bbd4b05bcf2a6fa7050688700d
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-07-10 20:52:47 -0500

    examples(vibrancy): handle vibrancy errors for demonstration

--- examples/vibrancy/src/main.rs
+++ examples/vibrancy/src/main.rs
@@ -11,36 +11,36 @@ struct AcrylicState {
 }
 
 #[millennium::command]
-fn toggle_mica(window: millennium::Window, state: millennium::State<AcrylicState>) {
-	let mut acrylic_state = state.acrylic.lock().unwrap();
+fn toggle_mica(window: millennium::Window, state: millennium::State<AcrylicState>) -> Result<(), String> {
+	let mut acrylic_state = state.acrylic.lock().map_err(|e| e.to_string())?;
 	if *acrylic_state {
-		clear_acrylic(&window);
+		clear_acrylic(&window).map_err(|e| e.to_string())?;
 		*acrylic_state = false;
 	}
 
-	let mut mica_state = state.mica.lock().unwrap();
+	let mut mica_state = state.mica.lock().map_err(|e| e.to_string())?;
 	*mica_state = !*mica_state;
 	if *mica_state {
-		apply_mica(&window);
+		apply_mica(&window).map_err(|e| e.to_string())
 	} else {
-		clear_mica(&window);
+		clear_mica(&window).map_err(|e| e.to_string())
 	}
 }
 
 #[millennium::command]
-fn toggle_acrylic(window: millennium::Window, state: millennium::State<AcrylicState>) {
-	let mut mica_state = state.mica.lock().unwrap();
+fn toggle_acrylic(window: millennium::Window, state: millennium::State<AcrylicState>) -> Result<(), String> {
+	let mut mica_state = state.mica.lock().map_err(|e| e.to_string())?;
 	if *mica_state {
-		clear_mica(&window);
+		clear_mica(&window).map_err(|e| e.to_string())?;
 		*mica_state = false;
 	}
 
-	let mut acrylic_state = state.acrylic.lock().unwrap();
+	let mut acrylic_state = state.acrylic.lock().map_err(|e| e.to_string())?;
 	*acrylic_state = !*acrylic_state;
 	if *acrylic_state {
-		apply_acrylic(&window, None);
+		apply_acrylic(&window, None).map_err(|e| e.to_string())
 	} else {
-		clear_acrylic(&window);
+		clear_acrylic(&window).map_err(|e| e.to_string())
 	}
 }
 
@@ -53,7 +53,7 @@ fn main() {
 		.invoke_handler(millennium::generate_handler![toggle_mica, toggle_acrylic])
 		.setup(|app| {
 			let main = app.get_window("main").unwrap();
-			apply_mica(&main);
+			apply_mica(&main)?;
 
 			Ok(())
 		})
