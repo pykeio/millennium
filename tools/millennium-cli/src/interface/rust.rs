01961fc04d25c2a1aa753523df4f767d4130825e
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-07-16 19:10:53 -0500

    feat(cli): allow dynamically linking vcruntime
    Upstream from tauri: 2e61aba

--- tools/millennium-cli/src/interface/rust.rs
+++ tools/millennium-cli/src/interface/rust.rs
@@ -160,7 +160,10 @@ impl Interface for Rust {
 		let bin_name = bin_path.file_stem().unwrap();
 
 		options.features.get_or_insert(Vec::new()).push("custom-protocol".into());
-		std::env::set_var("STATIC_VCRUNTIME", "true");
+
+		if !std::env::var("STATIC_VCRUNTIME").map_or(false, |v| v == "false") {
+			std::env::set_var("STATIC_VCRUNTIME", "true");
+		}
 
 		if options.target == Some("universal-apple-darwin".into()) {
 			std::fs::create_dir_all(&out_dir).with_context(|| "failed to create project out directory")?;
